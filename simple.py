62d9cacf55aa092f6229081b1274ec2c32c3cb7c
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2023-03-31 17:06:05 +0200

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+message = "helloooo"
+@app.route('/')
+def hello():
+    return 'Our message is {}.\n'.format(message)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
